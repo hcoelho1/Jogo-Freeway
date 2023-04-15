e14334e1939aca159823fdf4bf87556d6191c3ac
Henrique Coelho <68867483+hcoelho1@users.noreply.github.com>
2023-04-14 23:13:44 -0300

    Create Imagens-sons.js
    Criação das imagens e sons do jogo.

---
+++ Imagens-sons.js
@@ -0,0 +1,26 @@
+// Variáveis Imagens
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3; 
+
+
+// sons
+
+let somDaTrilha;
+let somDaColisao;
+let somDosPontos;
+
+
+function preload(){
+  imagemDaEstrada = loadImage("Imagens/estrada.png")
+  imagemDoAtor = loadImage("Imagens/ator-1.png")
+  imagemCarro = loadImage("Imagens/carro-1.png")
+  imagemCarro2 = loadImage("Imagens/carro-2.png")
+  imagemCarro3 = loadImage("Imagens/carro-3.png")
+  imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3];
+
+ somDaTrilha = loadSound("Sons/trilha.mp3");
+ somDaColisao = loadSound("Sons/colidiu.mp3")
+ somDoPonto= loadSound("Sons/pontos.wav");}
